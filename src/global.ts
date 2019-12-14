f4462882e3c8d8ea9d0b5742bdfb881d8dce0db5
Francois Gerthoffert <f.gerthoffert@gmail.com>
2019-12-14 11:26:39 -0500

    Started to add interfaces

--- src/global.ts
+++ src/global.ts
@@ -39,3 +39,34 @@ export interface Config {
   github: ConfigGithub;
   jira: Array<ConfigJira>;
 }
+
+interface User {
+  username: string;
+  fullname: string;
+  url?: string;
+  avatarUrl?: string;
+}
+
+interface Label {
+  name: string;
+  color: string;
+}
+
+// This interface aims at abstracting an issue at a level compatible across all ticketing systems
+export interface Issue {
+  createdAt: Date;
+  updatedAt: Date;
+  // In the absence of closed date in the source ticket schema, if the state of the issue is closed, closedAt will become the last time the issue was updated
+  closedAt: Date;
+  title: string;
+  description: string;
+  url: string;
+  states: 'OPEN' | 'IN-PROGRESS' | 'CLOSED';
+  assignee: Array<User>;
+  author: User;
+  labels: Array<Label>;
+  source: {
+    type: 'JIRA' | 'GITHUB';
+    data: any;
+  };
+}
